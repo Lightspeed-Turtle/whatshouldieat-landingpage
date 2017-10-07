375f9a1e598dd9266adff026cf4dc221336dca27
Travis Ali <ali.gilgil1@gmail.com>
2017-10-06 23:10:06 -0400

    New layout w/o footer

--- src/App.js
+++ src/App.js
@@ -35,41 +35,46 @@ class App extends Component {
   render() {
 
     const buttonToShow = (!this.state.isInstalled
-    ? <Button id="install-button" onClick={this.install.bind(this)} bsSize="large" bsClass='Install'>Essayez</Button> 
-    : <Button id="install-button" bsSize="large" bsClass='Installed' disabled>Ouvrez un nouvel onglet !</Button>);
+    ? <Button className="install-button" onClick={this.install.bind(this)} bsSize="large">Essayez</Button> 
+    : <Button className="install-button" bsSize="large" disabled>Ouvrez un nouvel onglet !</Button>);
 
     return (
-      <Grid className='App'>
-        <div className="page-wrap">
-          <Row>
-            <Col>
-              <img className="logo" src={logo} alt="What Should I Eat?" />
-            </Col>
-          </Row>
-          <Row className="content">
-            <Col lg={6} sm={12} style={{ padding: 0 }}>
-              <img className="image" src={laptop} alt="What Should I Eat?" />
-            </Col>
-            <Col className="text" lg={6} sm={12} style={{ textAlign: 'left' }}>
-              <h1>What Should I Eat?</h1>
-              <h2>Découvrez quoi manger une nouvelle recette alléchante et inspirante à l’ouverture de chaque nouvel onglet Chrome de votre ordinateur.</h2>
-              { 
-                this.state.isChrome 
-                  ? buttonToShow
-                  : <Button id="install-button" bsSize="large" bsClass='Installed' disabled>Cette application nécessite Chrome</Button>
-              }
-            </Col>
-          </Row>
-        </div>
-        {/* <Row className="footer">
-          <Col lg={6} >
-            <h3><a href="#">Share</a></h3>
-          </Col>
-          <Col lg={6}>
-            <h3>Une initiative de <a href="https://www.facebook.com/lightspeedturtle/">Lightspeed Turtle</a></h3>
-          </Col>
-        </Row> */}
-      </Grid>
+      <div style={{ height: "100%" }}>
+        <Grid className='App'>
+          <div className="page-wrap">
+            <Row className="logo-row">
+
+              <Col >
+                <img className="logo" src={logo} alt="What Should I Eat?" />
+              </Col>
+
+            </Row>
+
+            <Row className="content-row">
+
+              <Col lg={6} sm={12}>
+                <img className="image" src={laptop} alt="What Should I Eat?" />
+              </Col>
+
+              <Col className="text" lg={6} sm={12}>
+                <h1>What Should I Eat?</h1>
+                <h2>Découvrez quoi manger une nouvelle recette alléchante et inspirante à l’ouverture de chaque nouvel onglet Chrome de votre ordinateur.</h2>
+                { 
+                  this.state.isChrome 
+                    ? buttonToShow
+                    : <Button className="install-button" bsSize="large" disabled>Cette application nécessite Chrome</Button>
+                }
+              </Col>
+
+            </Row>
+          </div>
+        </Grid>
+        {/* <footer className="footer">
+          <div className="">
+            <p className="text-muted">Place sticky footer content here.</p>
+          </div>
+        </footer> */}
+      </div>
     );
   }
 }
