a0636a903e44fac7735d77037d82b0a63ee94cfc
Travis Ali <ali.gilgil1@gmail.com>
2017-10-06 21:49:34 -0400

    v2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import logo from './logo.png';
+import laptop from './laptop.png';
+
 import { Grid, Row, Col, Button } from 'react-bootstrap';
 
 class App extends Component {
@@ -32,33 +35,54 @@ class App extends Component {
   render() {
 
     const buttonToShow = (!this.state.isInstalled
-    ? <Button id="install-button" onClick={this.install.bind(this)} bsSize="large" bsClass='Install'>Installez</Button> 
+    ? <Button id="install-button" onClick={this.install.bind(this)} bsSize="large" bsClass='Install'>Essayez</Button> 
     : <Button id="install-button" bsSize="large" bsClass='Installed' disabled>Ouvrez un nouvel onglet !</Button>);
 
     return (
       <Grid className='App'>
-        <Row className="Title">
-          <Col>
-            <h1>Ne cherchez plus.<br /> Laissez les recettes venir à vous automatiquement et gratuitement à chaque nouvel onglet</h1>
+        <div className="page-wrap">
+          <Row>
+            <Col>
+              <img className="logo" src={logo} alt="What Should I Eat?" />
+            </Col>
+          </Row>
+          <Row className="content">
+            <Col lg={6} sm={12} style={{ padding: 0 }}>
+              <img className="image" src={laptop} alt="What Should I Eat?" />
+            </Col>
+            <Col className="text" lg={6} sm={12} style={{ textAlign: 'left' }}>
+              <h1>What Should I Eat?</h1>
+              <h2>Découvrez quoi manger une nouvelle recette alléchante et inspirante à l’ouverture de chaque nouvel onglet Chrome de votre ordinateur.</h2>
+              { 
+                this.state.isChrome 
+                  ? buttonToShow
+                  : <Button id="install-button" bsSize="large" bsClass='Installed' disabled>Cette application nécessite Chrome</Button>
+              }
+            </Col>
+          </Row>
+        </div>
+        {/* <Row className="footer">
+          <Col lg={6} >
+            <h3><a href="#">Share</a></h3>
           </Col>
-        </Row>
-        <Row className="Download">
-          <Col>
-            { 
-              this.state.isChrome 
-                ? buttonToShow
-                : <Button id="install-button" bsSize="large" bsClass='Installed' disabled>Cette application nécessite Chrome</Button>
-            }
+          <Col lg={6}>
+            <h3>Une initiative de <a href="https://www.facebook.com/lightspeedturtle/">Lightspeed Turtle</a></h3>
           </Col>
-        </Row>
-        <Row className="Footer">
-          <Col>
-            <h3>Créé et propulsé par <a href="https://www.facebook.com/lightspeedturtle/">Lightspeed Turtle</a></h3>
-          </Col>
-        </Row>
+        </Row> */}
       </Grid>
     );
   }
 }
 
 export default App;
+
+
+{/* <Row className="Download">
+<Col>
+  { 
+    this.state.isChrome 
+      ? buttonToShow
+      : <Button id="install-button" bsSize="large" bsClass='Installed' disabled>Cette application nécessite Chrome</Button>
+  }
+</Col>
+</Row> */}
